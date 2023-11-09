1e0576aea40f20746dc9911e1abf8bfb296b6581
Priyanshu Ratnakar <itzzzpriyanshu@gmail.com>
2023-11-09 07:35:27 +0530

    Update _app.tsx
    Open Graph meta tags updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,29 +13,18 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>priyanshuratnakar.com</title>
         <link rel="shortcut icon" href="fav.png" />
-        <meta property="og:title" content="priyanshu ratnakar" />
-        <meta
-          name="description"
-          content="researcher, weaving threads as a mordern day polymath"
-          key="desc"
-        />
-        <meta
-          property="og:image"
-          content="fav.png"
-        />
+        <meta property="og:title" content="Priyanshu Ratnakar" />
+        <meta name="og:description" content="researcher, weaving threads as a modern day polymath" key="desc"/>
+        <meta property="og:image" content="fav.png"/>
+        <meta property="og:url" content="https://www.priyanshuratnakar.com"/>
+        <meta property="og:type" content="website"/>
+
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:url" content="priyanshuratnakar.com/" key="twt-url" />
-        <meta name="twitter:title" content="ahzam" key="twt-title" />
-        <meta
-          name="twitter:description"
-          content="weaving threads as a mordern day polymath"
-          key="twt-desc"
-        />
-        <meta
-          name="twitter:image"
-          content="fav.png"
-          key="twt-img"
-        />
+        <meta name="twitter:title" content="Priyanshu Ratnakar" key="twt-title" />
+        <meta name="twitter:description" content="weaving threads as a modern day polymath" key="twt-desc" />
+        <meta name="twitter:image" content="fav.png" key="twt-img"/>
+
       </Head>
       <Analytics />
       <ChakraProvider>

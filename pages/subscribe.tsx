f224a99b809401d3127f3b53c431ac68dd43171d
Priyanshu Ratnakar <itzzzpriyanshu@gmail.com>
2023-12-28 02:30:21 +0530

    bugs fixes

--- pages/subscribe.tsx
+++ pages/subscribe.tsx
@@ -52,7 +52,7 @@ export default function Home() {
               </Text> 
               <Divider borderColor={"black"} />
               <Text>
-                subscribe below, and let&apos;s turn your inbox into a cozy corner for my write-ups, curated reading lists, book gems, resources, and more. spoiler alert &lsquo; it's gonna be fun. 📚✉️
+                subscribe below, and let&apos;s turn your inbox into a cozy corner for my write-ups, curated reading lists, book gems, resources, and more. spoiler alert &lsquo; it&apos;s gonna be fun. 📚✉️
               </Text>
             </Flex>
             </Flex>
